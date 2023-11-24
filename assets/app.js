2aea29af6cbe3e6067ee4db651ee274307da8dc5
AxelRaboit <axel.raboit@gmail.com>
2023-11-24 04:23:13 +0100

    [ADD] Tailwind + begin to remove bootstrap classes

--- assets/app.js
+++ assets/app.js
@@ -8,8 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './scss/app.scss';
 import './js/base/toast';
-import 'bootstrap';
-import bsCustomFileInput from 'bs-custom-file-input';
-bsCustomFileInput.init();
+import 'flowbite';
 
 const $ = require('jquery');
--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,12 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
+    .enablePostCssLoader((options) => {
+        options.postcssOptions = {
+            config: './postcss.config.js',
+        };
+    })
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
