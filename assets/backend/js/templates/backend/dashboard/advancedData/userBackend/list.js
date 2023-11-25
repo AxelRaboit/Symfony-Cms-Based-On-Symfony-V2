405cf12130e129249749ec08e0f048060ec3714d
AxelRaboit <axel.raboit@gmail.com>
2023-11-25 19:23:47 +0100

    [UPDATE] Improve back office search backend user form

--- assets/backend/js/templates/backend/dashboard/advancedData/userBackend/list.js
+++ assets/backend/js/templates/backend/dashboard/advancedData/userBackend/list.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('search-backend-user-input').value = '';
+    const searchFormInput = document.getElementById('search-backend-user-input-js');
+    const searchFormButton = document.getElementById('search-backend-user-submit-button-js');
+    const containerResetFormButton = document.getElementById('container-reset-button-js');
+    const resetFormButton = document.getElementById('reset-search-backend-user-submit-button-js');
+
+        searchFormInput.value = '';
+
+        searchFormInput.addEventListener('input', function (e) {
+            if (e.target.value.length > 0) {
+                containerResetFormButton.classList.remove('hidden');
+            } else {
+                containerResetFormButton.classList.add('hidden');
+            }
+        });
+
+    resetFormButton.addEventListener('click', function (e) {
+        searchFormInput.value = '';
+        containerResetFormButton.classList.add('hidden');
+    });
 });
