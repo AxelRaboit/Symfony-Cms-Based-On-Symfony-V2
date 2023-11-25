894296f95fd5ac064e86e6b42187284b1aae01b7
AxelRaboit <axel.raboit@gmail.com>
2023-11-25 22:30:02 +0100

    [UPDATE] Style Backend User List in Back Office

--- assets/backend/js/templates/backend/dashboard/advancedData/userBackend/list.js
+++ assets/backend/js/templates/backend/dashboard/advancedData/userBackend/list.js
@@ -2,17 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchForm = document.getElementById('search-backend-user');
     const searchFormInput = document.getElementById('search-backend-user-input-js');
     const containerResetFormButton = document.getElementById('container-reset-button-js');
-    const resetFormButton = document.getElementById('reset-search-backend-user-submit-button-js');
-
-    searchFormInput.value = '';
-
-    searchFormInput.addEventListener('input', function (e) {
-        if (e.target.value.length > 0) {
-            containerResetFormButton.classList.remove('hidden');
-        } else {
-            containerResetFormButton.classList.add('hidden');
-        }
-    });
+    const resetFormButton = document.getElementById('refresh-backend-user-list-js');
 
     resetFormButton.addEventListener('click', function (e) {
         searchFormInput.value = '';
