9229b60071eec823710fdc83d046a31849ef6672
AxelRaboit <axel.raboit@gmail.com>
2023-11-26 14:07:11 +0100

    [FEATURE] Backend Confirmation Popup

--- assets/backend/app.js
+++ assets/backend/app.js
@@ -1,5 +1,5 @@
-// any CSS you import will output into a single css file (app.css in this case)
 import './scss/app.scss';
+import './js/templates/backend/admin/base/elements';
 import 'flowbite';
 
 const $ = require('jquery');
---
+++ assets/backend/js/templates/backend/admin/base/elements/confirmation_popup.js
@@ -0,0 +1,37 @@
+// Using "windows." to make the function available in the global scope
+window.attachConfirmationToForm = function(formId, confirmationMessage) {
+    const form = document.getElementById(formId);
+    if (!form) return;
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        openPopup('Confirmation', confirmationMessage, function() {
+            form.submit();
+        });
+    });
+};
+
+function openPopup(title, message, onConfirm) {
+    const popup = document.getElementById('confirmation-popup');
+    const titleElement = document.getElementById('confirmation-popup-title');
+    const messageElement = document.getElementById('confirmation-popup-message');
+    const cancelButton = document.getElementById('confirmation-popup-cancel');
+    const confirmButton = document.getElementById('confirmation-popup-confirm');
+
+    titleElement.textContent = title;
+    messageElement.textContent = message;
+
+    cancelButton.addEventListener('click', closePopup);
+
+    confirmButton.addEventListener('click', function() {
+        closePopup();
+        if (onConfirm) onConfirm();
+    });
+
+    popup.classList.remove('hidden');
+}
+
+function closePopup() {
+    const popup = document.getElementById('confirmation-popup');
+    popup.classList.add('hidden');
+}
