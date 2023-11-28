f681b5e0b5cb5505ea8f29475a6668c85ab0b183
AxelRaboit <axel.raboit@gmail.com>
2023-11-28 14:11:11 +0100

    [UPDATE] Backend Dashboard

--- src/Controller/Backend/ApplicationData/UserApplicationController.php
+++ src/Controller/Backend/ApplicationData/UserApplicationController.php
@@ -2,6 +2,6 @@
 
 namespace App\Controller\Backend\ApplicationData;
 
-class UserFrontendController
+class UserApplicationController
 {
 }
--- src/Controller/Backend/BackendController.php
+++ src/Controller/Backend/BackendController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Backend;
 
+use App\Repository\DataEnumRepository;
+use App\Repository\PageRepository;
+use App\Repository\UserApplicationRepository;
+use App\Repository\UserBackendRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class BackendController extends AbstractController
 {
     #[Route('/backend/admin/dashboard', name: 'app_backend_dashboard')]
-    public function index(): Response
+    public function index(
+        PageRepository $pageRepository,
+        UserBackendRepository $userBackendRepository,
+        UserApplicationRepository $userApplicationRepository,
+        DataEnumRepository $dataEnumRepository
+    ): Response
     {
         return $this->render('backend/admin/dashboard/dashboard.html.twig', [
-            'controller_name' => 'BackendController',
+            'pageCount' => $pageRepository->count([]),
+            'userBackendCount' => $userBackendRepository->count([]),
+            'userApplicationCount' => $userApplicationRepository->count([]),
+            'dataEnumCount' => $dataEnumRepository->count([]),
         ]);
     }
 }
