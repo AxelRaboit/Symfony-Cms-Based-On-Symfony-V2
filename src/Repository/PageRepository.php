5cd9d4d6be2e06aac197474ad973de15da6c6e60
AxelRaboit <axel.raboit@gmail.com>
2023-12-17 14:48:38 +0100

    [FIX]

--- src/Repository/PageRepository.php
+++ src/Repository/PageRepository.php
@@ -326,7 +326,7 @@ class PageRepository extends ServiceEntityRepository
             SELECT p
             FROM App\Entity\Page p
             WHERE p.state = :state
-            AND p.isSeoNoFollow = 0
+            AND p.isSeoNoFollow = false
         ';
 
         $query = $this->getEntityManager()->createQuery($dql);
